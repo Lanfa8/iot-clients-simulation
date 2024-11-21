86852bad3854ed86d50872248ade0e90294d4b1a
Lanfa8 <alanfan139@gmail.com>
2024-11-20 22:49:37 -0300

    feat: clear comments

--- sensors/index.go
+++ sensors/index.go
@@ -9,26 +9,6 @@ import (
     "os"
 )
 
-// Valores mínimos (80% centrais):
-// temperatura: id=1398381, valor=11.3
-// umidade: id=2097, valor=46.0
-// luminosidade: id=893, valor=0.0
-// ruido: id=10573, valor=54.9
-// eco2: id=920, valor=0.0
-// etvoc: id=893, valor=400.0
-// latitude: id=3986277, valor=-29.379009
-// longitude: id=1649505, valor=-51.191895
-
-// Valores máximos (80% centrais):
-// temperatura: id=7392, valor=32.0
-// umidade: id=1363587, valor=94.6
-// luminosidade: id=40227, valor=806.7
-// ruido: id=1125, valor=88.9
-// eco2: id=134595, valor=334.0
-// etvoc: id=27244, valor=1698.0
-// latitude: id=893, valor=0.0
-// longitude: id=9511, valor=0.0
-
 func randWithMinMax(min, max float64) float64 {
     return min + rand.Float64() * (max - min)
 }
@@ -60,15 +40,6 @@ func getClient() mqtt.Client {
 
     return client
 }
-
-// func publish(client mqtt.Client) {
-//     num := 10
-//     for i := 0; i < num; i++ {
-//         text := fmt.Sprintf("Message %d", i)
-//         token := client.Publish("v1/devices/me/telemetry", byte(1), false, text)
-//         token.Wait()
-//     }
-// }
 
 type Measurement struct {
     Temperature float64 `json:"temperature"`
